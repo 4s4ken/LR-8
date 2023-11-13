a978b61e7e30543511fa2d34daa8cff252aa90c6
4skn <65232734+4s4ken@users.noreply.github.com>
2023-11-13 15:13:58 +0500

    Update LR-8.cpp
    Добавлен диалог с пользователем на ввод данных.

--- LR-8/LR-8/LR-8.cpp
+++ LR-8/LR-8/LR-8.cpp
@@ -1,4 +1,4 @@
-﻿#include<Windows.h>
+#include<Windows.h>
 #include<iostream>
 #include<algorithm>
 using namespace std;
@@ -7,18 +7,21 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	int N,s=0;
+	cout << "Введите кол-во сотрудников компании: " << endl;
 	cin >> N;
 	
 	int* A = new int[N];
 	int* B = new int[N];
 	int* C = new int[N];
 	int* D = new int[N];
+	cout << "Введите расстояние от работы до домов каждого из сотрудников: " << endl;
 	for (int i = 0;i < N;i++)
 	{
 		cin >> A[i];
 		C[i] = i;
 		D[i] = i;
 	}
+	cout << "Введите тарифлый план " << N << "-х таксистов: " << endl;
 	for (int i = 0;i < N;i++)
 		cin >> B[i];
 	for (int i = 0; i < N - 1; i++)
@@ -40,9 +43,9 @@ int main()
 		s += (A[i] * B[i]);
 		for (int j = 0;j < N;j++)
 			if (C[j] == i)
-				cout << "Таксисту под номером - " << i+1 << " нужно сесть в такси под номером: " << D[j] + 1 << endl;
+				cout << "Таксисту под номером - " << i + 1 << " нужно сесть в такси под номером: " << D[j] + 1 << endl;
 	}
-	cout << "." << endl;
+	cout << endl;
 	int num = s, digitcount = 0;
 	int k = num;
 	if (num % 10 == 1 && num % 100 != 11)
